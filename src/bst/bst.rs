8eebcb02ec3f27d2b3233e4bf17b345812aafb0d
dingari <danielgretars@bitbucket.org>
2016-03-31 13:15:29 +0000

    implemented a simple binary tree structure

---
+++ src/bst/bst.rs
@@ -0,0 +1,58 @@
+#[derive(Debug, Default)]
+pub struct BST<T> {
+    root: Option<Box<Node<T>>>
+}
+
+impl<T: PartialOrd> BST<T> {
+	pub fn insert(&mut self, new_val: T) {
+		let target_node = &mut self.root;
+		match target_node {
+			&mut Some(ref mut subnode) => {
+				subnode.insert(new_val);
+			},
+			&mut None => {
+				let new_node = Node::new(new_val);
+				let boxed_node = Some(Box::new(new_node));
+				*target_node = boxed_node;
+			}
+		}
+	}
+}
+
+#[derive(Debug, Default, PartialEq)]
+struct Node<T> {
+    val: T,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>
+}
+
+impl<T: PartialOrd> Node<T> {
+	pub fn new(new_val: T) -> Node<T> {
+		Node {
+			val: new_val,
+			left: None,
+			right: None
+		}
+	}
+
+    pub fn insert(&mut self, new_val: T) {
+    	if self.val == new_val {
+    		return
+    	}
+
+    	let target_node = if new_val < self.val {
+    		&mut self.left
+    	} else {
+    		&mut self.right
+    	};
+
+    	match target_node {
+    		&mut Some(ref mut subnode) => { subnode.insert(new_val) }
+    		&mut None => {
+    			let new_node = Node::new(new_val);
+    			let boxed_node = Some(Box::new(new_node));
+    			*target_node = boxed_node;
+    		}
+    	}
+    }
+}
