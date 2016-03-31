4449eacaa142027b76aefa58a90445e53d916b8c
dingari <danielgretars@bitbucket.org>
2016-03-31 10:16:52 +0000

    Created a struct for 2d vectors to make it nicer to work with tables

---
+++ src/vec2d/mod.rs
@@ -0,0 +1,3 @@
+mod vec2d;
+
+pub use self::vec2d::Vec2d;
---
+++ src/vec2d/vec2d.rs
@@ -0,0 +1,43 @@
+extern crate rand;
+
+use self::rand::Rng;
+use std::fmt;
+
+pub struct Vec2d<T> {
+    v: Vec<Vec<T>>
+}
+
+
+impl<T: Clone + Default + fmt::Debug> Vec2d<T> {
+    pub fn new(N: usize, M: usize) -> Vec2d<T> {
+    	let tmpV: Vec<Vec<T>> = vec![vec![T::default(); N]; M];
+    	Vec2d {
+    		v: tmpV
+    	}
+    }
+
+    pub fn get(&self, i: usize, j: usize) -> &T {
+    	&self.v[i][j]
+    }
+
+    pub fn set(&mut self, i: usize, j: usize, val: T) {
+    	self.v[i][j] = val;
+    }
+
+    pub fn len(&self) -> usize {
+    	self.v.len()
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for Vec2d<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    	for i in 0..self.v.len() {
+    		for j in 0..self.v[i].len() {
+    			write!(f, "{:?}, ", self.v[i][j]);
+    		}
+    		write!(f, "\n");
+    	}
+
+    	write!(f, "")
+    }
+}
